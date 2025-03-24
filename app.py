f7e6484d77e7c13d66947f069c81f5ed9d82b2b6
Murli Vishwakarma <134859656+murlivishwakarma@users.noreply.github.com>
2025-03-24 18:09:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def solve_captcha_endpoint():
     return jsonify({'captcha_text': captcha_text})
 
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic port
+  port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
 app.run(host="0.0.0.0", port=port, threaded=True)
